9e986269e8afb2795eb447d7c63b109d39ac0f7b
Osin Toluwani <toluwaniosin@gmail.com>
2026-09-08 18:28:56 +0100

    feat: bootstrap protected S3 bucket for Terraform state

---
+++ task-1-iac/bootstrap/main.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_version = ">= 1.10"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "eu-west-1"
+
+  default_tags {
+    tags = {
+      project     = "cloud-assessment"
+      environment = "learning"
+      owner       = "Toluwani"
+    }
+  }
+}

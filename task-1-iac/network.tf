a6b63413e69af761d982163c82950616643d519e
Osin Toluwani <osintoluwani@Osins-MacBook-Pro.local>
2026-09-08 16:02:35 +0100

    feat: add internet gateway and public subnet routing

--- task-1-iac/network.tf
+++ task-1-iac/network.tf
@@ -36,3 +36,30 @@ resource "aws_subnet" "private" {
     Name = "private-${count.index + 1}"
   }
 }
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "assessment-igw"
+  }
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main.id
+  }
+
+  tags = {
+    Name = "assessment-public-routes"
+  }
+}
+
+resource "aws_route_table_association" "public" {
+  count = 2
+
+  subnet_id      = aws_subnet.public[count.index].id
+  route_table_id = aws_route_table.public.id
+}

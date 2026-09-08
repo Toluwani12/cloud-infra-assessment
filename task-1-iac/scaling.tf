61367eacdf8613a367c9cf5bc7bfde7bff304f94
Osin Toluwani <osintoluwani@Osins-MacBook-Pro.local>
2026-09-08 16:15:01 +0100

    feat: scale ECS tasks on average CPU utilization

---
+++ task-1-iac/scaling.tf
@@ -0,0 +1,29 @@
+resource "aws_appautoscaling_target" "app" {
+  min_capacity = 2
+  max_capacity = 4
+
+  resource_id = "service/${aws_ecs_cluster.main.name}/${aws_ecs_service.app.name}"
+
+  scalable_dimension = "ecs:service:DesiredCount"
+  service_namespace  = "ecs"
+}
+
+resource "aws_appautoscaling_policy" "cpu" {
+  name        = "assessment-cpu-scaling"
+  policy_type = "TargetTrackingScaling"
+
+  resource_id        = aws_appautoscaling_target.app.resource_id
+  scalable_dimension = aws_appautoscaling_target.app.scalable_dimension
+  service_namespace  = aws_appautoscaling_target.app.service_namespace
+
+  target_tracking_scaling_policy_configuration {
+    target_value = 60
+
+    scale_out_cooldown = 60
+    scale_in_cooldown  = 120
+
+    predefined_metric_specification {
+      predefined_metric_type = "ECSServiceAverageCPUUtilization"
+    }
+  }
+}

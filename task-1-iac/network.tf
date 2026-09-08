5d36ecc33d3a5ed6170a48fdaef06c18bd1f552e
Osin Toluwani <osintoluwani@Osins-MacBook-Pro.local>
2026-09-08 16:04:23 +0100

    feat: add NAT gateways and private subnet routing

--- task-1-iac/network.tf
+++ task-1-iac/network.tf
@@ -63,3 +63,45 @@ resource "aws_route_table_association" "public" {
   subnet_id      = aws_subnet.public[count.index].id
   route_table_id = aws_route_table.public.id
 }
+resource "aws_eip" "nat" {
+  count  = 2
+  domain = "vpc"
+
+  tags = {
+    Name = "nat-ip-${count.index + 1}"
+  }
+}
+
+resource "aws_nat_gateway" "main" {
+  count = 2
+
+  allocation_id = aws_eip.nat[count.index].id
+  subnet_id     = aws_subnet.public[count.index].id
+
+  depends_on = [aws_internet_gateway.main]
+
+  tags = {
+    Name = "nat-${count.index + 1}"
+  }
+}
+
+resource "aws_route_table" "private" {
+  count  = 2
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.main[count.index].id
+  }
+
+  tags = {
+    Name = "private-routes-${count.index + 1}"
+  }
+}
+
+resource "aws_route_table_association" "private" {
+  count = 2
+
+  subnet_id      = aws_subnet.private[count.index].id
+  route_table_id = aws_route_table.private[count.index].id
+}
